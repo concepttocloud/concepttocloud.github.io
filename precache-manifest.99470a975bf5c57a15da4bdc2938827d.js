096b6d7513f580fd51614b0c09ca34e76ff3bf2e
concepttocloud <74042765+concepttocloud@users.noreply.github.com>
2020-11-06 02:50:40 -0500

    Initial commit

---
+++ precache-manifest.99470a975bf5c57a15da4bdc2938827d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf94c6e6c793a9d227e420a6c4964c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b668de14146ee475f51c",
+    "url": "/static/css/2.dca2bc21.chunk.css"
+  },
+  {
+    "revision": "eb642547dcf7f5306211",
+    "url": "/static/css/main.8fa32fb0.chunk.css"
+  },
+  {
+    "revision": "b668de14146ee475f51c",
+    "url": "/static/js/2.0ac03e6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0ac03e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb642547dcf7f5306211",
+    "url": "/static/js/main.1f51c598.chunk.js"
+  },
+  {
+    "revision": "7fad86708ce8eaa3b6b9",
+    "url": "/static/js/runtime-main.7ca2785d.js"
+  }
+]);
